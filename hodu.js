a5d8d9969cc420cf0a603a92d43ce33d5929fc15
dubu77 <139292883+dubu77@users.noreply.github.com>
2024-05-22 18:02:33 +0900

    Add files via upload

---
+++ hodu.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	//팝업모달
+    $("#subbtn").click(function(){
+        $("#popup").fadeIn();
+    });
+    //닫기
+    $("#popbtn").click(function(){
+        $("#popup").fadeOut();
+    });
+	
+});///// 끝
