2029689d5498bceda9f829141e23acb3d183bd34
dubu77 <hyamster37@naver.com>
2024-05-27 16:20:01 +0900

    add_more

--- hodu.js
+++ hodu.js
@@ -14,4 +14,8 @@ $(document).ready(function(){
         alert("아직 기능이 없습니다.");
         
     });
+    $(".top_menu a").click(function(){
+        alert("아직 기능이 없습니다.");
+        
+    });
 });///// 끝
